89dc59a370a6de851c4f662172df283e4579d300
kozir425 <maksonchikhasanshin@gmail.com>
2025-04-12 12:36:11 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,9 +2,9 @@
 
 int main() {
 
-  int red_balls[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int size = sizeof(red_balls) / sizeof(red_balls[0]);
-  printf("В коробке red_balls помещается %d шариков.\n", size);
+  int red_balls[10] = {1,1,1,1,1,1,1,1,1,1}; 
+    int red_count = sizeof(red_balls) / sizeof(red_balls[0]);
+    printf("Количество красных шариков: %d\n", red_count);
 
   return 0;
 }
