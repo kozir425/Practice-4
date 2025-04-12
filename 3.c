246499bea48ca05143e14177204b393d9ec7be2e
kozir425 <maksonchikhasanshin@gmail.com>
2025-04-12 12:39:07 +0300

    Update 3.c

--- 3.c
+++ 3.c
@@ -2,7 +2,7 @@
 
 int main() {
   
-  int colorful_balls[15] = {1,2,3,1,2,3,1,2,3,1,2,3,1,2,3}; 
+  int colorful_balls[15] = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1}; 
   int colorful_count = sizeof(colorful_balls) / sizeof(colorful_balls[0]);
   printf("Количество разноцветных шариков: %d\n", colorful_count);
   
